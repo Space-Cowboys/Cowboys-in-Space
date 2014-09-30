abfb4c122c548c5ce6e7a89b644b1de446fd3b09
Shari <Shari@192.168.1.148>
2014-09-29 23:33:12 -0400

    Pushed Inventory class. The class requires editing of MarketItem to function. It also is not bug tested for this very reason. I java doc'd it thoroughly. This class has a lot of flexibility. Notify for bug fixes.

--- src/Economy/economy/MarketItem.java
+++ src/Economy/economy/MarketItem.java
@@ -27,4 +27,55 @@ public class MarketItem {
     public int getHeight(){
         return height;
     }
+    
+    /*
+    IMPORTANT
+    In order for the Inventory class to work,
+    MarketItem must have several things.
+    */
+
+    public boolean isMaxed(){
+        //TO-DO returns true if the Amount == the Capcity
+        return false;
+    }
+
+
+
+    public int getAmount() {
+        //TO-DO: gets the amount of the item in the stack
+        return 0;
+    }
+
+
+    
+    public void setAmount(int amount){
+        //TO-DO: sets the amoutn of the items stack
+    }
+
+
+   
+    public int getCapacity() {
+        //TO-DO: returns max capacity of item's stack
+        return 0;
+    }
+
+
+
+    public boolean equals(MarketItem item) {
+        //TO-DO: checks to see if two items are the same regardless of items
+        // in the stack
+        return false;
+    }
+
+
+    
+    @Override
+    public String toString() {
+        //TO-DO: Returns the item name and the amount in the stack
+        return "";
+    }
+
+    
+
+    
 }
