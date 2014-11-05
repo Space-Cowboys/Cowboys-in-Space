b3142f85a3172eae5d0ae99806fea84bd87fd5dc
cdgallahue <cdgallahue@gmail.com>
2014-11-05 18:08:28 -0500

    Javadocs added

--- src/Space/POJO/PriceModel.java
+++ src/Space/POJO/PriceModel.java
@@ -4,12 +4,19 @@ import Space.POJO.Good.Goods;
 import Space.controller.RootLayoutController;
 import java.util.Random;
 
-
+/**
+ *
+ * @author Cameron
+ */
 public class PriceModel{
 	private final Planet location;
 	private final Random rand = new Random();
 	
-	public PriceModel(Planet location){
+    /**
+     *
+     * @param location
+     */
+    public PriceModel(Planet location){
 		this.location = location;
 	}
 	
@@ -25,19 +32,34 @@ public class PriceModel{
 		return (int)((rand.nextDouble()*(max-min))+min);
 	}
 	
-	public int showPrice(Goods good){
+    /**
+     *
+     * @param good
+     * @return
+     */
+    public int showPrice(Goods good){
             int initial = randRange(good.getMin(), good.getMax())*getGoodValue(good);
             int change = (int) ((int) initial * (RootLayoutController.getCharismaSkill() * .005));        
             return initial - change; 
 	} 
         
-        public int showSalePrice(Goods good) {
+    /**
+     *
+     * @param good
+     * @return
+     */
+    public int showSalePrice(Goods good) {
             int initial = randRange(good.getMin(), good.getMax())*getGoodValue(good) /2;
             int change = (int) ((int) initial * (RootLayoutController.getCharismaSkill() * .005));        
             return initial + change; 
         }
 	
-	public int showQuantity(Goods good){
+    /**
+     *
+     * @param good
+     * @return
+     */
+    public int showQuantity(Goods good){
 		return randRange(good.getMin(), good.getMax())*getGoodQuantity(good);
 	}
 }
--- src/Space/POJO/RandomEncounter.java
+++ src/Space/POJO/RandomEncounter.java
@@ -2,17 +2,34 @@ package Space.POJO;
 
 import java.util.Random;
 
+/**
+ *
+ * @author Jennifer
+ */
 public class RandomEncounter {
 
     private String startPlanet;
     private String endPlanet;
+
+    /**
+     *
+     */
     public final Random rand = new Random();
 
+    /**
+     *
+     * @param startPlanet
+     * @param endPlanet
+     */
     public RandomEncounter(String startPlanet, String endPlanet) {
         this.startPlanet = startPlanet;
         this.endPlanet = endPlanet;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean pirate() {
         int occurPercent = Universe.getUniverse().getFromName(startPlanet).getPirates()
             + Universe.getUniverse().getFromName(endPlanet).getPirates();
@@ -20,6 +37,10 @@ public class RandomEncounter {
         return occurPercent >= randNum;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean trader() {
         int occurPercent = Universe.getUniverse().getFromName(startPlanet).getTraders()
             + Universe.getUniverse().getFromName(endPlanet).getTraders();
@@ -27,6 +48,10 @@ public class RandomEncounter {
         return occurPercent <= randNum;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean police() {
         int occurPercent = Universe.getUniverse().getFromName(startPlanet).getPolice()
             + Universe.getUniverse().getFromName(endPlanet).getPolice();
